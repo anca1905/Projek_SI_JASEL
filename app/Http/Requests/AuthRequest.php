9ce5f2a42bc6cf7ab5f2f6831464fec273266fdd
anca1905 <arsyadhijrah49720@gmail.com>
2025-07-29 14:26:33 +0800

    create request Auth

---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6',
+            'password_confirmation' => 'required_with:password|same:password|min:6',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Email wajib diisi.',
+            'email.email' => 'Format email tidak valid.',
+            'password.required' => 'Password wajib diisi.',
+            'password.confirmed' => 'Konfirmasi password tidak cocok.',
+            'password.min' => 'Password minimal 6 karakter.',
+            'password_confirmation.required_with' => 'Konfirmasi password wajib diisi jika password diisi.',
+            'password_confirmation.same' => 'Konfirmasi password harus sama dengan password.',
+        ];
+    }
+}
