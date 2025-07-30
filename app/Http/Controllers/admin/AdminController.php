2c7ae089edff9c9fdc5e2abfc366443cc298a027
anca1905 <arsyadhijrah49720@gmail.com>
2025-07-30 15:02:02 +0800

    with out global scope for admin controller

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -8,12 +8,12 @@ use App\Models\Orders;
 class AdminController extends Controller
 {
     public function index(){
-        $data = Orders::all();
+        $data = Orders::withoutGlobalScopes()->get();
         return view('admin.index', compact('data'));
     }
 
     public function show($id){
-        $order = Orders::findOrFail($id);
+        $order = Orders::with('user', 'teknisi', 'manageService')->withoutGlobalScopes()->findOrFail($id);
         return view('admin.show', compact('order'));
     }
 
