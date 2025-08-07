06f933278ff1ef93fc1265bd2ae736caf1809094
anca1905 <arsyadhijrah49720@gmail.com>
2025-08-07 16:03:02 +0800

    feat(status): add new "menunggu_pembayaran" status on customer order view - Updated status color to orange using Tailwind CSS - Applied in costumer/order_history and make_an_order views

--- resources/views/costumer/make_an_order.blade.php
+++ resources/views/costumer/make_an_order.blade.php
@@ -98,7 +98,7 @@
                                                 'menunggu_konfirmasi' => 'bg-yellow-100 text-yellow-800',
                                                 'diproses' => 'bg-blue-100 text-blue-800',
                                                 'selesai' => 'bg-green-100 text-green-800',
-                                                'dibatalkan' => 'bg-red-100 text-red-800',
+                                                'menunggu_pembayaran' => 'bg-orange-100 text-orange-800',
                                                 default => 'bg-gray-100 text-gray-800',
                                             };
                                         @endphp
