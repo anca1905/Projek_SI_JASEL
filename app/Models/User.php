79cff772087501780526255d86a4d6e308cb75d5
anca1905 <arsyadhijrah49720@gmail.com>
2025-08-13 16:41:40 +0800

    feat(auth): add JWTSubject implementation to User model with identifier and custom claims

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,8 +11,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable, AdminTraits;
 
@@ -60,5 +61,20 @@ class User extends Authenticatable
     public function services()
     {
         return $this->belongsToMany(Services::class, 'technician_service', 'technician_id', 'service_id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
